06084c4c6e333cfb78ef8d0c52fc90faf4cbcc62
Mads Heimdal Thy <mads@heimdal-thy.dk>
2023-08-27 18:46:43 +0200

    a card for ticket details?

--- apps/tickets/src/views/Ticket.vue
+++ apps/tickets/src/views/Ticket.vue
@@ -16,6 +16,17 @@ onBeforeMount(async () => {
 
 <template>
   <RouterLink to="/">&larr; Tilbage til ticket listen</RouterLink>
+  <div class="card" style="width: 18rem">
+    <div class="card-body">
+      <h5 class="card-title">{{ ticket?.name }}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">ID: {{ ticket?.id }}</h6>
+      <p class="card-text">
+        {{ ticket?.description }}
+      </p>
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
